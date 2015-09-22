ec8980a074a6e22accb90357b98033b2c293fb91
LiuYu <290945322@qq.com>
2015-09-22 18:41:38 +0800

    增加一个activity

--- android/src/com/mygdx/game/android/AndroidLauncher.java
+++ android/src/com/mygdx/game/android/AndroidLauncher.java
@@ -5,12 +5,29 @@ import android.os.Bundle;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.mygdx.game.MyGdxGame;
+import com.tencent.tauth.Tencent;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		// Tencent类是SDK的主要实现类，开发者可通过Tencent类访问腾讯开放的OpenAPI。
+		// 其中APP_ID是分配给第三方应用的appid，类型为String。
+		//mTencent = Tencent.createInstance(APP_ID, this.getApplicationContext());
+		// 1.4版本:此处需新增参数，传入应用程序的全局context，可通过activity的getApplicationContext方法获取
+		// 初始化视图
+		//initViews();
+		
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new MyGdxGame(), config);
 	}
+/*	public void login()
+	{
+	mTencent = Tencent.createInstance(AppId, this.getApplicationContext());
+	if (!mTencent.isSessionValid())
+	{
+	mTencent.login(this, Scope, listener);
+	}
+	}*/
 }
